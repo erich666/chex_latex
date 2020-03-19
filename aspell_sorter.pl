31de7bd150cf9f089f78a6c64f4029b8648b62bc
Eric Haines <ehaines@nvidia.com>
2020-03-18 22:32:30 -0400

    Add CamelCase testing
    If a camel case word is found, split it into its component words. These will likely be spelled correctly, but they're easy enough to ignore. You also sometimes find something good.

--- aspell_sorter.pl
+++ aspell_sorter.pl
@@ -20,6 +20,9 @@
 # to print out only those words "misspelled" once, set $spellcount to 1; once or twice, set to 2, etc.
 my $spellcount = 0;
 
+# change this to whatever you want the minimum word length to be tested. Making this longer can help skip over two-letter variables, for example, e.g., "dx".
+my $wordlength = 0;
+
 while (@ARGV) {
 	# check 
 	$arg = shift(@ARGV) ;
@@ -43,7 +46,36 @@ sub READ {
 
 	while (<INFILE>) {
 		chop;       # strip record separator
-		$words{$_}++;
+		# clean out words with â in them - a flaw with aspell
+		# this is an apostrophe character, e.g, donâ is the "don" in don't
+		# That said, this test is a bit flaky, it doesn't catch them all, I don't know why.
+		if ( !/â/) {
+			# if a word is camel case, separate it into its constituent words
+			my $str = $_;
+			my $camelFound = 0;
+			while (length($str)>$wordlength) {
+				# throw away whatever is before the last capital letter in the camel case word (if there is one)
+				if ($str =~ /[A-Z]*([A-Z][a-z]+)/) {
+					# logic here's not quite right, but close enough...
+					if ( length($1) > $wordlength ) {
+						if ( length($') == 0 ) {
+							# if just a single capitalized word is found, save it without any prefix
+							$words{(($camelFound>1) ? '+ ' : '') . $1}++;
+						} else {
+							$words{(($camelFound>1) ? '+ ' : '* ') . $1}++;
+						}
+					}
+					$str = $';
+					$camelFound++;
+				} else {
+					#end loop
+					$str = '';
+				}
+			}
+			if (!$camelFound && (length($_) > $wordlength)) {
+				$words{$_}++;
+			}
+		}
 	}
 }
 
